b4689748d90240e4656174b1ccc03fb655c77cc5
KING-EZE <omojuwamiracle@gmail.com>
2022-09-29 22:49:38 +0000

    good

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
-def print_sorted_dictionary(a_dictionary):
-    for keys in sorted(a_dictionary.keys()):
-        print('{}: {}'.format(a_dictionary[keys], keys))
+def print_sorted_dictionary(a_dict):
+    """
+    print a dict in sorted order of keys
+    """
+    if a_dict is None:
+        return None
+    for key in sorted(a_dict.keys()):
+        print("{:s}: {}".format(key, a_dict[key]))
